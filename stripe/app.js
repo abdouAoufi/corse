cea9e57558c85ed036d6cc8353d4d63c6d397c4d
Abderhamnae Aoufi <abdouou7@gmail.com>
2020-10-17 15:24:38 +0100

    add stripe project full

---
+++ stripe/app.js
@@ -0,0 +1,33 @@
+import sublinks from './data.js';
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sidebarWrapper = document.querySelector(".sidebar-wrapper");
+const sidebar = document.querySelector(".sidebar-links");
+const linkBtns = [...document.querySelectorAll(".link-btn")];
+const submenu = document.querySelector(".submenu");
+const hero = document.querySelector(".hero");
+const nav = document.querySelector(".nav");
+
+
+toggleBtn.addEventListener("click", () => {
+    sidebarWrapper.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+    sidebarWrapper.classList.remove("show");
+});
+
+sidebar.innerHTML = sublinks.map(function(item) {
+            const { links, page } = item;
+            return `<article>
+    <h4> ${page} </h4>
+    <div class="sidebar-sublinks">
+     ${links.map(function(link){
+     return `<a href="${link.url}" > 
+     <i class="${link.icon}"></i>${link.label} 
+     </a>`;
+    }).join('')}
+    <div>
+    </article>`;
+}).join("");
